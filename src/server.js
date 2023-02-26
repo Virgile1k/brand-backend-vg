1047378a776f0caaa2779be1a5d30aae366b569c
KAYITARES <cyntkayitare@gmail.com>
2023-02-26 12:15:22 +0200

    running server

---
+++ src/server.js
@@ -0,0 +1,46 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import mongoose from "mongoose";
+
+mongoose.set("strictQuery", false);
+//config dotenv
+dotenv.config();
+
+//create app instance
+const app = express();
+
+//use an app instance
+app.use(cors());
+app.use(bodyParser.json());
+
+//Define port and host
+const host = process.env.HOST;
+const port = process.env.PORT;
+
+//morgan for logs
+if (process.env.NODE_ENV == "development") app.use(morgan);
+
+//our mongodb instance
+const con = () => {
+  mongoose.connect(process.env.DATABASE_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
+};
+
+//Listening to our server instance
+const startServer = () => {
+  app.listen(port);
+};
+
+Promise.all([con(), startServer()])
+  .then(() => {
+    console.log(`server running on ${port}`);
+    console.log(`database successfuly connected`);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
